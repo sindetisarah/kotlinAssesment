46111fece8cde2bbe87fbc46a068504207890809
sindetisarah <sindetisara@gmail.com>
2021-05-13 17:58:37 +0300

    second commit

--- src/assess.kt
+++ src/assess.kt
@@ -6,23 +6,23 @@ fun outputString(str:String):Int{
 var length=str.length
     return length
 }
-data class Car(var make:String,var model:String,var color:String){
- fun checkColor(car: List<Car>)  {
-     var purple= mutableListOf<Car>()
-     var red= mutableListOf<Car>()
-     var orange= mutableListOf<Car>()
-     var car= mutableListOf<Car>(
-         Car("benz","car","red"),
-     Car("benz","me","blue"),
-     Car("benz","toyota","puple")
-     )
-     when(car.color){
-         "purple"=>purple.add(car)
-         "red"->red.add(car)
-         "orange"->orange.add(car)
-     }
-
- }}
+//data class Car(var make:String,var model:String,var color:String){
+// fun checkColor(car: List<Car>)  {
+//     var purple= mutableListOf<Car>()
+//     var red= mutableListOf<Car>()
+//     var orange= mutableListOf<Car>()
+//     var car= mutableListOf<Car>(
+//         Car("benz","car","red"),
+//     Car("benz","me","blue"),
+//     Car("benz","toyota","puple")
+//     )
+//     when(car.color){
+//         "purple"=>purple.add(car)
+//         "red"->red.add(car)
+//         "orange"->orange.add(car)
+//     }
+//
+// }}
 
 
 class CurrentAccount(var accountNumber:String,var accountName:String,var balance:Int){
@@ -37,6 +37,14 @@ class CurrentAccount(var accountNumber:String,var accountName:String,var balance
     }
 
 }
-class savingsAccount(){
+class SavingsAccount(var accountNumber:String,var accountName:String,var balance:Int,var withdrawals:Int){
+    var count=0;
+     fun withdraw(savingsAccount.withdraw){
+         if (count<4){
+             count ++;
 
+         }
+        println("allow to withdaraw ${count}")
+
+     }
 }
